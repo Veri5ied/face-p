db5b8fda1d053d50a69cee083009717a492c53c9
ceceukoha <ceceukoha@gmail.com>
2023-08-24 10:46:19 +0100

    ui cleanup

--- components/WritePost.js
+++ components/WritePost.js
@@ -1,54 +1,62 @@
 import { useState } from 'react';
-import Image from 'next/image'
+import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront'
 import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
 import SentimentVerySatisfiedIcon from '@mui/icons-material/SentimentVerySatisfied';
-import { Button,TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import { db } from '@/settings/firebase.setting';
-import { collection,addDoc } from 'firebase/firestore'
-import { async } from '@firebase/util';
+import { collection, addDoc } from 'firebase/firestore';
+import { cdnImages } from '@/assets/demo_cdn_images';
+// import { rangeOfRandNums } from 'assets/range-of-rand-nums';
+import { rangeOfRandNums } from '@/assets/range-of-rand-Nums';
 
 export default function WritePost() {
-    const {data:session} = useSession();
-    const [formInput,setFormInput] = useState('');
+    const { data: session } = useSession();
+    const [formInput, setFormInput] = useState('');
 
-    //create post to firestore 
+    // create post to firestore
     const handleCreatePost = async () => {
-        await addDoc(collection(db,'posts'),{
-            body:formInput,
-            author:session.user.email,
-            postedAt:new Date().getTime(),
-
+        await addDoc(collection(db, 'posts'), {
+            body: formInput,
+            author: session.user.email,
+            postedAt: new Date().getTime(),
+            imageUrl: cdnImages[rangeOfRandNums(0, cdnImages.length)]
         })
-        .then(() => alert('Your post was published'))
-        .catch(error => console.error(error));
+            .then(() => {
+                setFormInput('');
+                alert('Your post was published');
+            })
+            .catch(error => console.error(error))
     }
-    
+
     return (
-        <div className="flex flex-col border border-gray-100 bg-white rounded-md shadow-md p-3 mb-4 gap-4">
-            <div className='flex flex-row justify-between items-center gap-4'>
-                <Image 
-                className="rounded-full" 
-                width={48} 
-                height={48}
-                src={session?.user.image} 
-                alt="profile photo" />
+        <form className="flex flex-col border border-gray-100 bg-white rounded-md shadow-md p-3 mb-4 gap-4">
+            <div className='flex flex-row items-center gap-4'>
+                <Image
+                    className="rounded-full"
+                    width={48}
+                    height={48}
+                    src={session?.user.image}
+                    alt="profile photo" />
 
-                <form className='w-full flex flex-col gap-2'>
-                    <TextField 
-                    multiline={true}
-                    className='w-full'
-                    value={formInput}
-                    onChange={(text) => setFormInput(text.target.value)}/>
+                <div className='w-full flex flex-col gap-2'>
+                    <TextField
+                        multiline={true}
+                        className='w-full'
+                        placeholder="what's on your mind ..."
+                        value={formInput}
+                        onChange={(text) => setFormInput(text.target.value)} />
 
-                    <Button 
-                    variant='outlined'
-                    className=''
-                    onClick={handleCreatePost}>Post</Button>
-                </form>
+                    {formInput.length > 0
+                        ? <Button
+                            variant='outlined'
+                            className='block w-[100px]'
+                            onClick={handleCreatePost}>Post</Button>
+                        : null}
+                </div>
             </div>
-            <hr style={{color:'black'}}/>
+            {/* <hr style={{ color: 'black' }} />
 
             <div className='flex flex-row justify-around  gap-4'>
                 <button className='w-full p-2 hover:bg-gray-200 text-gray-500 rounded'>
@@ -63,7 +71,7 @@ export default function WritePost() {
                     <SentimentVerySatisfiedIcon sx={{ color: 'yellow' }} />
                     Feelig/activity
                 </button>
-            </div>
-        </div>
+            </div> */}
+        </form>
     )
 }
--- pages/feeds.js
+++ pages/feeds.js
@@ -1,63 +1,86 @@
-import React from 'react'
-import Image from 'next/image'
-import { GoSignOut } from 'react-icons/go';
-import { useSession,signOut } from 'next-auth/react';
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import WritePost from '@/components/WritePost';
+import { getDocs, collection } from 'firebase/firestore';
+import { db } from '@/settings/firebase.setting';
+import PostDisplay from '@/components/PostDisplay';
 
+/*
+REMOVE
+1. live video, et al block
+2. share from post
+3. X form top of post
+*/
 
-
-export default function () {
-    const {data:session} = useSession();
+export default function Feeds() {
+    const { data: session } = useSession();
+    const [posts, setPosts] = useState([]);
     const router = useRouter();
-
-    console.log(session);
 
     React.useEffect(() => {
         if (!session) {
             router.push('/auth/signup')
         }
-    },[]);
+    }, []);
 
-  return (
-    <>
-        <main 
-        className="h-screen relative flex flex-col items-center justify-center pt-20 bg-scroll bg-gray-200">
-            <nav className="w-full h-19 flex flex-row justify-between items-center fixed top-0 left-0 right-0 bg-white shadow-md p-2">
-                <div className="flex items-center justify-center w-[50px] h-[50px] rounded-full bg-gray-200" >
-                    <Image 
-                    width={40} 
-                    height={40} 
-                    src='/facepal_logo.png' 
-                    alt="facepal logo" />
+    //get posts from firestore
+    const getPosts = async () => {
+        const res = await getDocs(collection(db, 'posts'));
+
+        setPosts(res.docs.map(doc => {
+            return {
+                id: doc.id,
+                data: {
+                    ...doc.data()
+                }
+            }
+        }))
+    }
+    getPosts();
+
+    return (
+        <>
+            <main className="h-screen flex justify-center bg-gradient-to-b from-indigo-500 via-sky-500 to-pink-500">
+                <div className="w-full sm:w-[400px] h-full bg-white overflow-y-scroll">
+                    {/* profile holder */}
+                    <header className="flex flex-row justify-between bg-indigo-300 p-3 shadow-md">
+                        <Image
+                            width={140}
+                            height={58}
+                            className="w-auto"
+                            src="/facepal_logo.png"
+                            alt="profile photo" />
+
+                        <Image
+                            className="rounded-full"
+                            width={58}
+                            height={58}
+                            src={session?.user.image}
+                            alt="profile photo" />
+                    </header>
+
+                    <div className="flex flex-col gap-2 p-3">
+                        <WritePost />
+
+                        {/* previous posts holder */}
+                        <div className="flex flex-col gap-2">
+                            {
+                                posts.map(post => (
+                                    <div id={post.id}>
+                                        <PostDisplay
+                                            timePosted={post.data.postedAt}
+                                            body={post.data.body}
+                                            postImage={post.data.imageUrl}
+                                        />
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
                 </div>
-
-                <GoSignOut 
-                className='text-gray-800'
-                onClick={signOut}/>
-
-                <Image 
-                className="rounded-full" 
-                width={48} 
-                height={48}
-                src={session?.user.image} 
-                alt="profile photo" />
-            </nav>
-            <div className="w-full sm:w-[600px] h-full overflow-y-scroll px-4 no-scrollbar">
-            {/*feeds holder*/}
-
-                <WritePost />
-
-                {/* previous posts holder */}
-                <div className="flex flex-col gap-2">
-
-                    {/* single post  */}
-                        
-                    {/* end of first post */}
-
-                </div>
-            </div>
-        </main>
-    </>
-  )
+            </main>
+        </>
+    )
 }
