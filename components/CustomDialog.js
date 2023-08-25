d240e3f99b66c03520bb310d198d90434baa8131
ceceukoha <ceceukoha@gmail.com>
2023-08-25 16:18:09 +0100

    added dialog

---
+++ components/CustomDialog.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+const CustomDialog = ({openProp, handleCloseProp, title, children }) => {
+    return (
+        <div>
+            <Dialog
+                open={openProp}
+                onClose={handleCloseProp}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {title}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {children}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseProp}>Dismiss</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
+
+export default CustomDialog;
